17af0f6e15db5a314d8bd4ead5cbab1c7e2ebe7e
henkepro <henkepro@hotmail.com>
2025-02-18 16:05:01 +0200

    as

---
+++ css/About-us.js
@@ -0,0 +1,8 @@
+var map = document.getElementById ('mymap'); 
+var maplistner = function (e) { 
+    var frame = document.createElement ('iframe'); 
+    frame.src = this.getAttribute ('data-src'); 
+    map.appendChild (frame); 
+    map.removeEventListener ("mouseover", maplistner); 
+}; 
+map.addEventListener ('mouseover', maplistner);
