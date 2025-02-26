6cd3e90a1abcae621f73e3b7a39a19e2079ed763
henkepro <henkepro@hotmail.com>
2025-02-26 15:08:32 +0100

    dqw

--- css/About-us.js
+++
@@ -1,8 +0,0 @@
-var map = document.getElementById ('mymap'); 
-var maplistner = function (e) { 
-    var frame = document.createElement ('iframe'); 
-    frame.src = this.getAttribute ('data-src'); 
-    map.appendChild (frame); 
-    map.removeEventListener ("mouseover", maplistner); 
-}; 
-map.addEventListener ('mouseover', maplistner);
--- css/Product.js
+++ css/Product.js
@@ -1,45 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Get all pizzas, drinks, and desserts containers and the form container
     let pizzas = document.getElementsByClassName("pizzas-item");
     let drinks = document.getElementsByClassName("drinks-item");
     let desserts = document.getElementsByClassName("desserts-item");
 
-    // Selecting the individual select dropdowns by their unique IDs
     let pizzaSelect = document.getElementById("pizzaSelect");
     let drinkSelect = document.getElementById("drinkSelect");
     let dessertSelect = document.getElementById("dessertSelect");
 
-    // Convert HTMLCollection to array for pizzas
     Array.from(pizzas).forEach((pizza) => {
-        let pizzaName = pizza.querySelector("p").innerText; // Get the pizza name from the <p> tag
+        let pizzaName = pizza.querySelector("p").innerText;
 
         let option = document.createElement("option");
-        option.value = pizzaName;  // Set pizza name as value
-        option.innerText = pizzaName;  // Display pizza name in the dropdown
+        option.value = pizzaName;
+        option.innerText = pizzaName;
 
-        pizzaSelect.appendChild(option); // Append the option to the pizza select dropdown
+        pizzaSelect.appendChild(option);
     });
 
-    // Similar loop for drinks (you can add more logic here)
     Array.from(drinks).forEach((drink) => {
-        let drinkName = drink.querySelector("p").innerText; // Get drink name
+        let drinkName = drink.querySelector("p").innerText;
 
         let option = document.createElement("option");
-        option.value = drinkName;  // Set drink name as value
-        option.innerText = drinkName;  // Display drink name in the dropdown
+        option.value = drinkName;
+        option.innerText = drinkName;
 
-        drinkSelect.appendChild(option); // Append the option to the drink select dropdown
+        drinkSelect.appendChild(option);
     });
 
-    // Similar loop for desserts (you can add more logic here)
     Array.from(desserts).forEach((dessert) => {
-        let dessertName = dessert.querySelector("p").innerText; // Get dessert name
+        let dessertName = dessert.querySelector("p").innerText;
 
         let option = document.createElement("option");
-        option.value = dessertName;  // Set dessert name as value
-        option.innerText = dessertName;  // Display dessert name in the dropdown
+        option.value = dessertName;
+        option.innerText = dessertName;
 
-        dessertSelect.appendChild(option); // Append the option to the dessert select dropdown
+        dessertSelect.appendChild(option);
     });
 });
 
